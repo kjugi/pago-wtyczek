3e1c5644f19cd6585e6d825d967782aaefe7065a
kjugi <filipkud31@gmail.com>
2017-01-07 20:29:07 +0100

    add background.js file in manifest.json
    basic script which checking each x seconds stream

--- background.js
+++ background.js
@@ -6,13 +6,68 @@ Kappa
 #PAPUT
 */
 
-function getTwitchStreamStatus(){
+function getTwitchStreamStatus(streamOn, streamOff, errorCallback){
 	//one streamer extension PAGO3
 	//demo mode: on
 	//https://api.twitch.tv/kraken/streams/40378922?client_id=dlw882cebptkltr33a4s0ejzongxfg
 	var streamerID = "40378922";
 	var client_id = "dlw882cebptkltr33a4s0ejzongxfg";
 	var url = "https://api.twitch.tv/kraken/streams/";
+  	var data = null;
 
-	var fullUrl = url+streamerID+"?client_id="+client_id;
+	var xhr = new XMLHttpRequest();
+
+	xhr.open("GET", url+streamerID);
+	xhr.setRequestHeader("accept", "application/vnd.twitchtv.v5+json");
+	xhr.setRequestHeader("client-id", client_id);
+	xhr.setRequestHeader("cache-control", "no-cache");
+	xhr.setRequestHeader("Content-Type", "application/json; charset=utf-8");
+
+  	xhr.onload = function() {
+	    var response = JSON.parse(xhr.responseText);
+
+	    if (!response) {
+	      errorCallback('No response from TWITCH API!');
+	      return;
+	    }
+
+	    if(response.stream != null){
+		    var streamTitle = response.stream.channel.status;
+		    var streamGame = response.stream.game;
+		    var streamLiveViewers = response.stream.viewers;
+		    var streamLiveDate = response.stream.created_at;
+		    var streamPreviewMedium = response.stream.preview.medium;
+
+		    console.assert(
+		        typeof response == 'object', 'Unexpected response from the TWITCH API!');
+		    chrome.browserAction.setIcon({path: "icons/icon_1.png"});
+		    streamOn(streamTitle, streamGame, streamLiveViewers, streamLiveDate, streamPreviewMedium);
+		}
+		else{
+			var streamNull = "Brak streama!";
+
+			console.assert(
+				typeof response == 'object', 'Unexpected response from the TWITCH API!');
+			chrome.browserAction.setIcon({path: "icons/icon_default.png"});
+			streamOff(streamNull);
+		}
+	};
+	xhr.onerror = function() {
+	    errorCallback('Network error.');
+	};
+	xhr.send(data);
 }
+
+setInterval(function(){
+	getTwitchStreamStatus(function(streamTitle, streamGame, streamLiveViewers, streamLiveDate, streamPreviewMedium) {
+		chrome.browserAction.setIcon({path: "icons/icon_1.png"});
+	},
+	function(streamOff){
+		chrome.browserAction.setIcon({path: "icons/icon_default.png"});
+	},
+	function(errorMessage) {
+	  console.log('Cannot display information. ' + errorMessage);
+	});
+},
+10000	
+);
